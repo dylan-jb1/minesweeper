4e6ea776e9e35d32a0546c5f3de6a5666ea3b317
dylan-jb1 <89441511+dylan-jb1@users.noreply.github.com>
2021-10-20 21:43:14 +1000

    Version 0.1.0
    Humble beginnings

---
+++ class.py
@@ -0,0 +1,13 @@
+class Level:
+    def __init__(self):
+        self.map = list()
+        pass
+
+    def refresh(self):
+        pass
+
+    def move(self, mousePos):
+        pass
+
+class Button:
+    def __init__(self, x, y, width, height)
